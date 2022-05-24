52a724a5d5bc32f1810cc76a7fd88e68bc740593
salgu1998 <salgu1998@gmail.com>
2022-05-25 00:21:49 +0900

    add : custom filter 등록

---
+++ src/main/java/com/apigatewayservice/filter/CustomFilter.java
@@ -0,0 +1,36 @@
+package com.apigatewayservice.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+public class CustomFilter extends AbstractGatewayFilterFactory<CustomFilter.Config> {
+
+    public CustomFilter() {
+        super(Config.class);
+    }
+
+    public static class Config {
+        // Put the configuration properties.
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        // Custom pre filter
+        return (exchange, chain) -> {
+            final ServerHttpRequest request = exchange.getRequest();
+            final ServerHttpResponse response = exchange.getResponse();
+
+            log.info("Custom Pre filter - req ID = {}", request.getId());
+            return chain.filter(exchange).then(Mono.fromRunnable(() ->
+                    log.info("Custom Post filter - res code = {}", response.getStatusCode())
+            ));
+        };
+    }
+}
